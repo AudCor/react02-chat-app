a7e2f6cda4570bc57f4455141a36e2c393e7abab
AudCor <clapyourhands4ever@gmail.com>
2019-10-14 22:57:08 +0200

    ajout des contacts de la quête précédente dans ContactList

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,51 +1,55 @@
-import React from 'react';
-import Contact from './Contact';
+import React from "react";
+import Contact from "./Contact";
 
-
-const usersInfos = [{
+const usersInfos = [
+  {
     name: "Potté",
     avatar: "https://i.ibb.co/zfFHSVD/chat-potte.jpg",
-    online: true
+    online: true,
   },
   {
     name: "Cheshire Cat",
     avatar: "https://i.ibb.co/RB7rygn/cheshire-cat.jpg",
-    online: false
+    online: false,
   },
   {
     name: "Chi",
     avatar: "https://i.ibb.co/fvxc5x1/Chi.png",
-    online: true
+    online: true,
   },
   {
     name: "Lucifer",
     avatar: "https://i.ibb.co/2gKJqCn/lucifer.jpg",
-    online: true
+    online: true,
   },
   {
     name: "Raoul Chatigré",
     avatar: "https://i.ibb.co/xYkJv5J/Raoul-Chatigre.jpg",
-    online: false
-  }
-  /*, {
-  name: "Garfield",
-  avatar: "https://i.ibb.co/QrZvPJf/proxy-duckduckgo-com.jpg",
-  online
-}, {
-  name: "Attila",
-  avatar: "https://i.ibb.co/ZXDtqD2/attila.png",
-}, {
-  name: "Tom",
-  avatar: "https://i.ibb.co/h2LTssM/tom.jpg",
-  online
-}*/
+    online: false,
+  },
+  {
+    name: "Garfield",
+    avatar: "https://i.ibb.co/QrZvPJf/proxy-duckduckgo-com.jpg",
+    online: true,
+  },
+  {
+    name: "Attila",
+    avatar: "https://i.ibb.co/ZXDtqD2/attila.png",
+    online: false,
+  },
+  {
+    name: "Tom",
+    avatar: "https://i.ibb.co/h2LTssM/tom.jpg",
+    online: true,
+  },
 ];
 
 const ContactList = () => {
-  return usersInfos.map((user) => {
-return <Contact name={user.name} avatar={user.avatar} online={user.online} />
-})
+  return usersInfos.map(user => {
+    return (
+      <Contact name={user.name} avatar={user.avatar} online={user.online} />
+    );
+  });
 };
-
 
 export default ContactList;
