1d21eeb5b38bf6889bf785a894a661034bf6d77e
AudCor <clapyourhands4ever@gmail.com>
2019-10-22 12:05:02 +0200

    Transformation de Contact en class et utilisation de props.online pour initialiser le statut online/offline

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,43 @@
 import React from "react";
 import "./contact.css";
-import Status from './Status';
+import Status from "./Status";
 
-const Contact = (props) => {
-  return (
-    <div className="Contact">
-      <div className="avatar">
-        <img src={props.avatar} alt={props.name} className="avatarBox" />
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
+  }
+
+  render() {
+    //   const toggleStatus =  () => {
+    //     const newStatus = !this.state.online;
+    //     this.setState({online: newStatus});
+    // }
+
+    return (
+      <div className="Contact">
+        <div className="avatar">
+          <img
+            src={this.props.avatar}
+            alt={this.props.name}
+            className="avatarBox"
+          />
+        </div>
+        <div className="infos">
+          <h3 className="name"> {this.props.name} </h3>
+          <Status
+            online={this.state.online}
+            toggleStatus={() => {
+              const newStatus = !this.state.online;
+              this.setState({ online: newStatus });
+            }}
+          />
+        </div>
       </div>
-      <div className="infos">
-        <h3 className="name"> {props.name} </h3> 
-        <Status online={props.online} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contact;
--- src/components/Status.js
+++ src/components/Status.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+
 const Status = (props) => {
   return (
-    <div className="status">
+    <div className="status" onClick={props.toggleStatus}>
       <div className={props.online ? "status-online" : "status-offline"} />
       <p className="status-text"> {props.online ? "online" : "offline"} </p>
     </div>
